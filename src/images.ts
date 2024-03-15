32c426660c01b3ea5682f8f5393b6d187b542fbc
Jakub Buksa <jbuksa1906@gmail.com>
2024-03-15 19:34:00 +0100

    Update images.ts

--- src/images.ts
+++ src/images.ts
@@ -1,3 +1,7 @@
+import first from '../src/assets/firstHero.jpg'
+import sec from '../src/assets/secondHero.jpg'
+import third from '../src/assets/thirdHero.jpg'
+
 type TSlider = {
     backgroundImage: string
     backgroundID: number
@@ -7,21 +11,21 @@ type TSlider = {
 
 export const slider: TSlider[] = [
     {
-        backgroundImage: 'public/firstHero.jpg',
+        backgroundImage: first,
         backgroundID: 0,
         header: `OD ZERA \n POD KLUCZ`,
         paragraph:
             'Świadczymy usługi w zakresie kompleksowych \n wykończeń mieszkań od stanu deweloperskiego \n do stanu “pod klucz”, gołe “4 ściany” zamienimy \n w przytulne mieszkanie.',
     },
     {
-        backgroundImage: 'public/secondHero.jpg',
+        backgroundImage: sec,
         backgroundID: 1,
         header: `REMONTUJEMY \n PRYWATNE MIESZKANIA`,
         paragraph:
             'Swoje usługi kierujemy do Klientów prywatnych, \n którzy szukają ekipy do remontu lub wykończenia swojego „M”.',
     },
     {
-        backgroundImage: 'public/thirdHero.jpg',
+        backgroundImage: third,
         backgroundID: 2,
         header: `PRACUJEMY \n W MAŁYCH DRUŻYNACH`,
         paragraph:
