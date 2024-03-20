8fee5aae5d7b7dc87444df88148a84468e92c993
Jakub Buksa <jbuksa1906@gmail.com>
2024-03-20 17:22:19 +0100

    update

--- src/components/Realizations/Realizations.tsx
+++ src/components/Realizations/Realizations.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+import React, { useEffect, useState } from 'react'
+
+/* import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry' */
 
 type Props = {}
-
 export default function Realizations({}: Props) {
+    const [image, setImage] = useState(null)
+
     return <div>Realizations</div>
 }
