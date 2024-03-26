e55a72d3c4549d29954fcba1549656f132df3c9f
Jakub Buksa <jbuksa1906@gmail.com>
2024-03-26 18:21:59 +0100

    update

--- src/components/Realizations/Realizations.tsx
+++ src/components/Realizations/Realizations.tsx
@@ -1,9 +1,24 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 /* import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry' */
 
 type Props = {}
+
 export default function Realizations({}: Props) {
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const data = await axios.get(
+                    `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_API_NAME}/resources`
+                )
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchImages()
+    }, [])
     const [image, setImage] = useState(null)
 
     return <div>Realizations</div>
