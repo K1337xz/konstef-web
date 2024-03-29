8daf77c49ec9a4fa6e9e39b11a7d5e059e4b896a
Jakub Buksa <jbuksa1906@gmail.com>
2024-03-29 18:23:55 +0100

    update

--- src/components/Realizations/Realizations.tsx
+++ src/components/Realizations/Realizations.tsx
@@ -1,17 +1,44 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { imagesRef } from '@/firebase/config'
-/* import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry' */
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/storage'
+import { firebaseConfig } from '@/firebase/config'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
 
 type Props = {}
 
 export default function Realizations({}: Props) {
-    const [data, setData] = useState([])
-    const [image, setImage] = useState(null)
+    const [data, setData] = useState<string[]>([])
 
     useEffect(() => {
-        console.log(imagesRef)
-    }, [])
-
-    return <div>Realizations</div>
+        const fetchImages = async () => {
+            firebase.initializeApp(firebaseConfig)
+            const storageRef = firebase.storage().ref()
+            const imagesRef = storageRef.child('images')
+            try {
+                const imageUrls: string[] = []
+                const listResult = await imagesRef.listAll()
+                listResult.items.forEach(async (itemRef) => {
+                    const imageUrl = await itemRef.getDownloadURL()
+                    imageUrls.push(imageUrl)
+                })
+                setData(imageUrls)
+            } catch (error) {
+                console.error('Error fetching images:', error)
+            }
+        }
+        fetchImages()
+    }, [setData])
+    console.log(data)
+    return (
+        <div className="flex w-full flex-col items-start gap-10 p-6 text-center md:text-left lg:py-32 lg:text-left">
+            <div className="w-full">
+                <h1 className="w-full text-center font-bebasNeue text-4xl  text-csk-600 md:text-center lg:px-40 lg:text-left lg:text-3xl">
+                    Realizacje
+                </h1>
+            </div>
+            <div className="w-full">
+                <div className="flex flex-col items-center gap-7  lg:flex-row lg:pl-40"></div>
+            </div>
+        </div>
+    )
 }
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,21 +1,15 @@
 import { initializeApp } from 'firebase/app'
-import { getStorage, listAll, ref } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCFWtiPLleh_PqDoEjYkTktMEqYSyN_MiI',
-    authDomain: 'konstef-420ab.firebaseapp.com',
-    projectId: 'konstef-420ab',
-    storageBucket: 'konstef-420ab.appspot.com',
-    messagingSenderId: '1084081248700',
-    appId: '1:1084081248700:web:dd1149d7689391e67cb3b9',
-    measurementId: 'G-J7XLZ1ENRV',
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDERID,
+    appId: import.meta.env.VITE_FIREBASE_APPID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
 }
 
 const app = initializeApp(firebaseConfig)
-const storage = getStorage()
-const sorageRef = ref(storage)
 
-const imagesRef = ref(storage, 'images/')
-
-const getListOfImages = async () => {}
-export { imagesRef }
+export { firebaseConfig }
