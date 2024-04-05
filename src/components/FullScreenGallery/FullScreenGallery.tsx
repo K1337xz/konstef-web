c39f6cf9ef8adcadfa531636b60e87a4c845d3ec
Jakub Buksa <jbuksa1906@gmail.com>
2024-04-05 15:41:52 +0200

    update

--- src/components/FullScreenGallery/FullScreenGallery.tsx
+++ src/components/FullScreenGallery/FullScreenGallery.tsx
@@ -1,20 +1,23 @@
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+
 type Props = {
     mainImg?: string
-    thumbNails: string[]
+    /*     thumbNails: string[]
+    data: () => void */
 }
 
-export default function FullScreenGallery({ mainImg, thumbNails }: Props) {
+export default function FullScreenGallery({ mainImg }: Props) {
     return (
         <div className="fixed top-0 h-full w-full bg-csk-900/95">
             <div className="flex w-full flex-col items-center justify-center gap-4 lg:px-40 lg:py-5">
                 <div className="flex h-1/2 w-full items-center justify-center">
                     <img src={mainImg} className="w-4/12" />
                 </div>
-                <div className="flex w-full items-center justify-center gap-10">
-                    {thumbNails?.map((itm) => (
-                        <img src={itm} className="w-24" />
-                    ))}
-                </div>
+                <div
+                    className="no-scrollbar flex w-full items-center justify-center gap-10 overflow-scroll
+                "
+                ></div>
             </div>
         </div>
     )
--- src/components/Realizations/Realizations.tsx
+++ src/components/Realizations/Realizations.tsx
@@ -1,40 +1,68 @@
-import { useEffect, useState } from 'react'
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/storage'
-import { firebaseConfig } from '@/firebase/config'
-import FramerDiv from '../FramerDiv/FramerDiv'
+import { useEffect, useState, useRef } from 'react'
 import { useInView } from 'react-intersection-observer'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import FramerDiv from '../FramerDiv/FramerDiv'
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+import firebase from 'firebase/compat/app'
+import { firebaseConfig } from '@/firebase/config'
 import FullScreenGallery from '../FullScreenGallery/FullScreenGallery'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+
+import 'firebase/compat/storage'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
 type Prop = {}
 
 export default function Realizations({}: Prop) {
+    const galleryRef = useRef<HTMLImageElement | null | undefined>()
     const { ref: ref, inView: showMasonry } = useInView()
-    const [data, setData] = useState<string[]>([])
+    const [data, setData] = useState<string[][]>([])
+    const [images, setImages] = useState<string[]>([])
+    const [dataIndex, setDataIndex] = useState<number>(0)
     const [clickedImage, setClickedImage] = useState<string>()
+
+    const splitArray = (arr: string[], size: number) => {
+        const result: string[][] = []
+        let index = 0
+        while (index < arr.length) {
+            result.push(arr.slice(index, index + size))
+            index += size
+        }
+        console.log(result)
+        return result
+    }
+
+    const fetchMoreImages = () => {
+        let loadImages = [...images, ...data[1]]
+        setImages(loadImages)
+    }
+
     useEffect(() => {
         firebase.initializeApp(firebaseConfig)
         const storageRef = firebase.storage().ref()
         const imagesRef = storageRef.child('images')
-        const imageUrls: string[] = []
+
         const fetchImages = async () => {
             try {
                 const listResult = await imagesRef.listAll()
-                listResult.items.forEach(async (itemRef) => {
-                    const imageUrl = await itemRef.getDownloadURL()
-                    imageUrls.push(imageUrl)
-                })
+                const imageUrls: string[] = []
+
+                await Promise.all(
+                    listResult.items.map(async (itemRef) => {
+                        const imageUrl = await itemRef.getDownloadURL()
+                        imageUrls.push(imageUrl)
+                    })
+                )
+                const splittedArray = splitArray(imageUrls, 10)
+                setData(splittedArray)
+                setImages(splittedArray[0])
             } catch (error) {
                 console.error('Error fetching images:', error)
-            } finally {
-                setData(imageUrls)
             }
         }
+
         fetchImages()
     }, [])
-
-    const imagesSliced = data.slice(0, 6)
 
     return (
         <>
@@ -52,36 +80,43 @@ export default function Realizations({}: Prop) {
                         </h1>
                     </div>
                     <div className="w-full">
-                        <div className="flex flex-col items-center gap-7  lg:flex-row lg:px-40 lg:py-32">
+                        <div className="flex flex-col items-center justify-center gap-7  lg:flex-row lg:px-40 lg:py-32">
                             {showMasonry && (
-                                <ResponsiveMasonry
-                                    columnsCountBreakPoints={{
-                                        350: 1,
-                                        768: 2,
-                                        900: 3,
-                                    }}
-                                    className="w-full"
+                                <InfiniteScroll
+                                    dataLength={data.length}
+                                    next={fetchMoreImages}
+                                    hasMore={true}
+                                    loader={<li>load</li>}
+                                    style={{ width: '100vh' }}
                                 >
-                                    <Masonry columnsCount={3} gutter="10px">
-                                        {imagesSliced.map((itm, i) => (
-                                            <img
-                                                src={itm}
-                                                key={i}
-                                                onClick={() => {
-                                                    setClickedImage(itm)
-                                                }}
-                                            />
-                                        ))}
-                                    </Masonry>
-                                </ResponsiveMasonry>
+                                    <ResponsiveMasonry
+                                        columnsCountBreakPoints={{
+                                            350: 1,
+                                            768: 2,
+                                            900: 3,
+                                        }}
+                                        className="w-full"
+                                    >
+                                        <Masonry columnsCount={3} gutter="10px">
+                                            {images?.map((itm, i) => (
+                                                <LazyLoadImage
+                                                    src={itm}
+                                                    key={i}
+                                                    onClick={() => {
+                                                        setClickedImage(itm)
+                                                    }}
+                                                    effect="blur"
+                                                />
+                                            ))}
+                                        </Masonry>
+                                    </ResponsiveMasonry>
+                                </InfiniteScroll>
                             )}
                         </div>
                     </div>
                 </FramerDiv>
             </div>
-            {clickedImage && (
-                <FullScreenGallery mainImg={clickedImage} thumbNails={data} />
-            )}
+            {clickedImage && <FullScreenGallery mainImg={clickedImage} />}
         </>
     )
 }
