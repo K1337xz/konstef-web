709bd46891219d04a675d143595942a6d1db7ac7
Jakub Buksa <108631492+K1337xz@users.noreply.github.com>
2024-05-20 16:28:08 +0200

    update

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -9,7 +9,7 @@ export function Contact ({}: Props) {
         <h1 className="w-full text-center font-bebasNeue text-4xl  text-csk-600 md:text-center lg:px-40 lg:text-left lg:text-3xl">
            Kontakt
         </h1>
-        <p className='w-full text-opacity-0 flex'>Kontakt telefoniczny i wizyta przed podpisaniem umowy to dla nas chleb powszedni. Po wstępnych rozmowach i wycenie podpisujemy umowę, wchodzimy na miejsce prac, niezwłocznie zabierając się do pracy.
+        <p className='w-full text-opacity-0 '>Kontakt telefoniczny i wizyta przed podpisaniem umowy to dla nas chleb powszedni. Po wstępnych rozmowach i wycenie podpisujemy umowę, wchodzimy na miejsce prac, niezwłocznie zabierając się do pracy.
 Jeśli masz więcej pytań, zachęcamy do kontaktu!</p>
    </div>
 </div>
