1d520e447b4a760e94ef97f566b2848c951a5b71
Jakub Buksa <108631492+K1337xz@users.noreply.github.com>
2024-05-20 16:12:11 +0200

    update

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,17 @@
+
+
+type Props = {}
+
+export function Contact ({}: Props) {
+  return (
+    <div className="flex w-full flex-col items-start gap-10 p-6 text-center md:text-left lg:py-32 lg:text-left">
+    <div className="w-full">
+        <h1 className="w-full text-center font-bebasNeue text-4xl  text-csk-600 md:text-center lg:px-40 lg:text-left lg:text-3xl">
+           Kontakt
+        </h1>
+        <p className='w-full text-opacity-0 flex'>Kontakt telefoniczny i wizyta przed podpisaniem umowy to dla nas chleb powszedni. Po wstępnych rozmowach i wycenie podpisujemy umowę, wchodzimy na miejsce prac, niezwłocznie zabierając się do pracy.
+Jeśli masz więcej pytań, zachęcamy do kontaktu!</p>
+   </div>
+</div>
+  );
+}
